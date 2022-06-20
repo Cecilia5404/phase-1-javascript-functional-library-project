ce6b960900d9b66390cbeb8b88b38b8accb2b215
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-06-20 07:40:28 +0300

    commit tree f8c73aa3910f69c87c8886077b4cbf82d5ac9096

---
+++ index.js
@@ -0,0 +1,13 @@
+function myeach(testArr, alert) {
+    const  objValues = object.values(testArr)
+    return  objValues.forEach(test => {
+        alert(test)
+    })
+    return testArr;
+}
+function myMap(testArr,callback) {
+    const objValues = object.values(testArr)
+    callback = testArr.map(test => {
+        
+    })
+}
